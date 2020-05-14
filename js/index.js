897866b1f484b84c4aaf7afaa43c1fbdf631b958
migguz53 <guzman.miguel2000@gmail.com>
2020-05-13 15:25:02 -1000

    aboutpage

--- js/index.js
+++ js/index.js
@@ -7,16 +7,3 @@ var slider = tns({
 
 console.log("load");
 // right_image
-
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.innerHTML === "Hello") {
-    x.innerHTML = "Swapped text!";
-  } else if (x.innerHTML === "Swapped text!") {
-    x.innerHTML = "Kill Me";
-  } else if (x.innerHTML === "Kill Me") {
-    x.innerHTML = "Number 4";
-  } else {
-    x.innerHTML = "Hello";
-  }
-}
